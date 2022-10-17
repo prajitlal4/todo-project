2321b8864d5bcba764e9478ed9937b8ed56dc218
prajitlal <prajit.lal4@gmail.com>
2022-10-17 11:27:27 +0800

    add tasks functionality working

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import { useState } from 'react'
 import './App.css';
 
 const App = () => {
-  const [tasks, addTasks] = useState([
+  const [tasks, setTasks] = useState([
     { id: 1, name: "Do some coding", completed: false },
     { id: 2, name: "Take out the trash", completed: false}
   ])
+  const [newTask, setNewTask] = useState('')
+
+  const handleTaskChange = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const addTask = (event) => {
+    event.preventDefault()
+    setTasks(tasks.concat({id: tasks.length+1, name: newTask, completed: false}))
+    setNewTask('')
+  }
 
   return (
     <div>
       <h1 className="text-3xl">To do app</h1>
       <p>Add todo item</p>
-      <form>
+      <form onSubmit={addTask}>
         <div>
-          Task name: <input />
+          Task name: <input value={newTask} onChange={handleTaskChange}/>
         </div>
         <div>
           <button type="submit">Add</button>
